876c41785b5186b382bb8260382a8a68ef40e461
David Calcus <targoo@gmail.com>
2021-06-13 20:49:26 +0100

    fix stoybook

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+## Knows issues
+
+- https://github.com/styleguidist/react-docgen-typescript/issues/356 (upgrade to typescript)
--- src/components/atoms/Backdrop/Backdrop.tsx
+++ src/components/atoms/Backdrop/Backdrop.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { FC, ReactNode, CSSProperties } from 'react';
 import styled from 'styled-components';
 
-export function Backdrop({ opacity, color, children }) {
+type BackdropProps = {
+  children: ReactNode;
+  opacity: number;
+  color: string;
+};
+
+interface CSSPropertiesWithVars extends CSSProperties {
+  '--color': string;
+  '--opacity': number;
+}
+
+export const Backdrop: FC<BackdropProps> = ({ children, opacity, color }) => {
   return (
     <Wrapper
-      style={{
-        '--color': color,
-        '--opacity': opacity,
-      }}
+      style={
+        {
+          '--color': color,
+          '--opacity': opacity,
+        } as CSSPropertiesWithVars
+      }
     >
       {children}
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   color: var(--color);

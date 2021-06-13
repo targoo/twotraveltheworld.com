9f0311c5e5775601f1f9c477e4b387c10373e4aa
David Calcus <targoo@gmail.com>
2021-06-13 21:32:22 +0100

    move pages to src folder

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  i18n: {
+    locales: ['en-UK', 'fr'],
+    defaultLocale: 'en-UK',
+    localeDetection: true,
+    // domains: [
+    //   {
+    //     domain: 'example.com',
+    //     defaultLocale: 'en-US',
+    //   },
+    //   {
+    //     domain: 'example.nl',
+    //     defaultLocale: 'nl-NL',
+    //   },
+    //   {
+    //     domain: 'example.fr',
+    //     defaultLocale: 'fr',
+    //   },
+    // ],
+  },
+  productionBrowserSourceMaps: false,
+};
